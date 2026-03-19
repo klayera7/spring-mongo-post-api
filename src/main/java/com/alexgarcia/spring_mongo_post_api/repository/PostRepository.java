b894ab170f7a86eaee922570005d13788628b149
Alex Kauã Silva Garcia <alexkaua502@gmail.com>
2026-03-19 17:52:22 -0300

    feat: implement custom MongoDB query with regex for case-insensitive title search

--- src/main/java/com/alexgarcia/spring_mongo_post_api/repository/PostRepository.java
+++ src/main/java/com/alexgarcia/spring_mongo_post_api/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.alexgarcia.spring_mongo_post_api.repository;
 import com.alexgarcia.spring_mongo_post_api.domain.Post;
 import com.alexgarcia.spring_mongo_post_api.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
