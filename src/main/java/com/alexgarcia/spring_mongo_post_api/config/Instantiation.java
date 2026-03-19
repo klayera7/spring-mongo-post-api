cec963c26936ead73235880849eb14376f800f15
Alex Kauã Silva Garcia <alexkaua502@gmail.com>
2026-03-19 16:38:19 -0300

    feat: create Post domain entity with nested User author and repository layer

--- src/main/java/com/alexgarcia/spring_mongo_post_api/config/Instantiation.java
+++ src/main/java/com/alexgarcia/spring_mongo_post_api/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.alexgarcia.spring_mongo_post_api.config;
 
+import com.alexgarcia.spring_mongo_post_api.domain.Post;
 import com.alexgarcia.spring_mongo_post_api.domain.User;
+import com.alexgarcia.spring_mongo_post_api.repository.PostRepository;
 import com.alexgarcia.spring_mongo_post_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,17 +18,28 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo, abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei bem, me senti bem!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
 
